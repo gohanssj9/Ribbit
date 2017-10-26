78c5214a90385913bcfbcb885dbd157c2f015172
gohanssj9 <majinvegito01@gmail.com>
2017-10-26 10:53:39 +0530

    Implemented User Profiles and Following Relation on frontend

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
+urlpatterns = patterns(
+    '',
     url(r'^$', 'ribbit_app.views.index'), # root
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^signup$', 'ribbit_app.views.signup'), # signup
     url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
     url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
-    #url(r'^users/$', 'ribbit_app.views.users'),
-]
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'),
+)
